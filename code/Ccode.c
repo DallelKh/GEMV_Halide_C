c2d4e47144a2fcb8567baa6553856dc589d55b5c
DallelKh <gd_khezzane@esi.dz>
2020-06-19 22:03:48 +0100

    v

--- code/Ccode.c
+++ code/Ccode.c
@@ -1,40 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Global Declarations
+
+    int n,m; //Dimensions of Matrix and Vector
+    FILE *input; //  pointer to file that contains Matrix,Vector Values
+    FILE *output; //pointer to file that contains Halide Result
+
+void retriveData(int A[n][m],int X[n]){
+    int temp,i,j;
+    // filling A
+
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j <n; j++){
+            fscanf(input,"%d", &temp);
+            A[i][j]=temp;
+        }
+    }
+    //filling X
+
+    for (j = 0; j <n; j++){
+        //get row
+            fscanf(input,"%d", &temp);
+            X[j]=temp;
+    }
+    return ;
+
+}
 
 
+void gemv(int A[m][n],int X[n],int Y[m]){
 
+    int i,j;
+    for (i = 0; i < m; i++){ Y[i]=0;
+        for (j = 0; j <n; j++){
+            Y[i] += A[i][j] * X[j];
+        }
+    }
+    return ;
+}
+
+ int validate_halide(int Y[m]){
+        int j,temp;
+        int H[m];
+
+        //Retrieving results calculated by halide implementation
+        output=NULL;
+        output = fopen("..\\files\\in.txt","r");
+        if (output == NULL){
+               printf("\nError! can't open output file");
+               exit(1);
+            }
+         for (j = 0; j <m; j++){
+                //get  int
+                fscanf(output,"%d", &temp);
+                H[j]= temp;
+
+                if(Y[j]!=H[j])
+                    return 0;
+               }
+           fclose(output);
+        return 1;
+ }
 
 int main()
-{
-    int i,j;
-    double value[10000][3];
-    FILE *input;
-    input = fopen("input.txt","r");
-    if ((fptr = fopen("C:\\program.txt","r")) == NULL){
-       printf("Error! opening file");
 
-       // Program exits if problem openning file.
+{    //opening Input file
+    input = fopen("..\\files\\input.txt","r");
+    if (input == NULL){
+       printf("\nError! opening file");
        exit(1);
-   }
-    i=0;
-    while (feof(archivo) == 0)
-    {
-        fscanf( archivo, "%lf %lf %lf", &value[i][0],&value[i][1],&value[i][2]);
-        printf("%10.0f %f %f\n", value[i][0], value[i][1], value[i][2]);
-        i++;
     }
 
-    for (i = 0; i < rows; i++)
-    {   Y[i]=0
-	for (j = 0; j < cols; j++)
-         {
-        Y[i] += A[i][jfclose(input); ] * X[j];
-
-	}
-    }
+    //getting dimensions n,m
+    fscanf(input,"%d", &m);
+    fscanf(input,"%d", &n);
 
 
-    fclose(input);
-    return 0;
+   //Declaring and allocating  matrix A and vectors X,Y
+    int A[m][n];
+    int X[n];
+    int Y[m];
+
+   //filling A and X from file
+   retriveData(A,X);
+   fclose(input);
+
+   //Calculating Y = A*X
+   gemv(A,X,Y);
+
+   //Checking with Halide Results
+   if(validate_halide(Y))
+    printf("\nResults Match\n");
+    else printf("\nResults Do Not Match!\n");
+
+   return 0;
 }
+
---
+++ run.sh
@@ -0,0 +1,4 @@
+echo "Compiling ./code/Ccode.c "
+gcc ./code/Ccode.c -o ./exe/Cexe
+echo "Executing C implementation "
+./exe/Cexe
